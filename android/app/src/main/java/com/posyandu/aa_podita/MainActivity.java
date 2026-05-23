b579d833cd952deac1d4a93d98601d270377c29d
poor-aren <iil.ilham241@gmail.com>
2026-05-23 13:00:57 +0700

    Add files via upload

---
+++ android/app/src/main/java/com/posyandu/aa_podita/MainActivity.java
@@ -0,0 +1,6 @@
+package com.posyandu.aa_podita;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/posyandu/aa_podita/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.posyandu.aa_podita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
